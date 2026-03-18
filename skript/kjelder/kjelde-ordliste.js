43db5bcbdd456491e0899e2e087ddd65f269d52d
tlauen <torbjorn@lauen.no>
2026-03-18 22:34:11 +0100

    Update kjelde-ordliste.js

--- skript/kjelder/kjelde-ordliste.js
+++ skript/kjelder/kjelde-ordliste.js
@@ -4,7 +4,7 @@
 window.KJELDE_ORDLISTE = {
   "ANTUN": {
     tittel: "Vestlandsspringdans",
-    tekst: "Antun, Markvard (1967)",
+    tekst: "Markvard Antun (1967)",
     url: "https://www.nb.no/items/ef698f1e3ce61df3c37e2955cf76bbe7",
     nb: "https://www.nb.no/items/ef698f1e3ce61df3c37e2955cf76bbe7",
     start: ""
@@ -18,42 +18,42 @@ window.KJELDE_ORDLISTE = {
   },
   "DDL": {
     tittel: "Danse, danse, lett ut på foten : folkedansar og songdansar",
-    tekst: "Bakka, Egil (1970)",
+    tekst: "Egil Bakka (1970)",
     url: "https://www.nb.no/items/7e9ac65479a3545bfcc06630900c8c9a",
     nb: "https://www.nb.no/items/7e9ac65479a3545bfcc06630900c8c9a",
     start: 2
   },
   "DDDM": {
     tittel: "Danse, danse dokka mi : songdansar - turdansar - ringleikar - turleikar : for born og ungdom",
-    tekst: "Semb, Klara (1958)",
+    tekst: "Klara Semb (1958)",
     url: "https://www.nb.no/items/34ae9cabf321fe4a06ee1820f506bd92",
     nb: "https://www.nb.no/items/34ae9cabf321fe4a06ee1820f506bd92",
     start: 2
   },
   "DMD": {
     tittel: "Danse mi dokke mæ du æ ung : 10 songdansar",
-    tekst: "Stallvik, Tor (1982)",
+    tekst: "Tor Stallvik (1982)",
     url: "pdf/Dansemidokke-Tor-Stallvik.pdf",
     nb: "",
     start: ""
   },
   "DT:F": {
     tittel: "Dansetradisjonar frå Finnmark",
-    tekst: "Bakka, Egil; Seland, Brit; Vårdal, Dag (1986)",
+    tekst: "Egil Bakka og Arne Wikan (1996)",
     url: "https://www.nb.no/items/c786de2a23cb9f5c422f92ff31c6a08e",
     nb: "https://www.nb.no/items/c786de2a23cb9f5c422f92ff31c6a08e",
     start: 2
   },
   "DT:NT": {
     tittel: "Dansetradisjonar frå Nordland og Troms",
-    tekst: "Bakka, Egil; Vårdal, Dag; Lunde, Tormod (1984)",
+    tekst: "Egil Bakka, Dag Vårdal og Tormod Lunde (1984)",
     url: "https://www.nb.no/items/e0428dd3c3e2605d31ef6b85fa7bfa60",
     nb: "https://www.nb.no/items/e0428dd3c3e2605d31ef6b85fa7bfa60",
     start: 6
   },
   "DT:VA": {
     tittel: "Dansetradisjonar frå Vest-Agder",
-    tekst: "Bakka, Egil; Egeland, Ånon; Vårdal, Dag; Seland, Brit (1990)",
+    tekst: "Egil Bakka, Dag Vårdal og Brit Seland; Slåttar ved Ånon Egeland  (1990)",
     url: "https://www.nb.no/items/URN:NBN:no-nb_digibok_2015062508077",
     nb: "https://www.nb.no/items/URN:NBN:no-nb_digibok_2015062508077",
     start: 2
@@ -81,15 +81,15 @@ window.KJELDE_ORDLISTE = {
   },
   "GIF": {
     tittel: "Grunnbok i folkedans",
-    tekst: "Bakka, Egil; Wikan, Arne (1996)",
+    tekst: "Egil Bakka, Brit Seland og Dag Vårdal (1991)",
     url: "https://www.nb.no/items/d17cd9d064011fb25db86b18f184035d",
     nb: "https://www.nb.no/items/d17cd9d064011fb25db86b18f184035d",
     start: 0
   },
   "HID": {
     tittel: "Haralds illustrerte danseviser",
-    tekst: "Aadnevik, Harald (2002)",
-    url: "https://folkedans.com",
+    tekst: "Harald Aadnevik (2002)",
+    url: "",
     nb: "",
     start: ""
   },
@@ -102,21 +102,21 @@ window.KJELDE_ORDLISTE = {
   },
   "KARI": {
     tittel: "Kari går i ringen : Songleikar, selskapsleikar, rim og regler frå Inn-Trøndelag",
-    tekst: "Bjerkem, Berit Eldbjørg (1990)",
+    tekst: "Berit Eldbjørg Bjerkem (1990)",
     url: "https://www.nb.no/maken/item/URN:NBN:no-nb_digibok_2018050848081",
     nb: "https://www.nb.no/maken/item/URN:NBN:no-nb_digibok_2018050848081",
     start: 0
   },
   "ND": {
     tittel: "Norske dansevisur",
-    tekst: "Garborg, Hulda (1913)",
+    tekst: "Hulda Garborg (1913)",
     url: "https://www.nb.no/items/5b4c39b69c7804615452aac4f29b0052",
     nb: "https://www.nb.no/items/5b4c39b69c7804615452aac4f29b0052",
     start: 4
   },
   "NF:S": {
     tittel: "Norske folkedansar: Songdansar",
-    tekst: "Semb, Klara (1991)",
+    tekst: "Klara Semb (1991)",
     url: "https://songdans.no",
     // Nb.no-lenke til boka. Brukast av lenker i tabellen "Rettleiing"
     // (page= blir rekna ut frå sidetal + start).
@@ -127,7 +127,7 @@ window.KJELDE_ORDLISTE = {
   },
   "NF:T": {
     tittel: "Norske folkedansar: Songdansar",
-    tekst: "Semb, Klara (1985)",
+    tekst: "Klara Semb (1985)",
     url: "https://turdans.no",
     nb: "https://www.nb.no/items/URN:NBN:no-nb_digibok_2007111904027",
     start: 0
@@ -148,14 +148,14 @@ window.KJELDE_ORDLISTE = {
   },
   "PRØYSEN": {
     tittel: "D'er æiller fali for den som dæinse : Songdans til viser av Alf Prøysen",
-    tekst: "Tuko, Ingrid Maurstad; Slapgård, Nils Steinar (2009)",
+    tekst: "Ingrid Maurstad Tuko og Nils Steinar Slapgård (2009)",
     url: "",
     nb: "",
     start: ""
   },
   "TRØND.": {
     tittel: "Trønderviser",
-    tekst: "Larsen, Geir Egil (1984)",
+    tekst: "Geir Egil Larsen (1984)",
     url: "",
     nb: "",
     start: ""
